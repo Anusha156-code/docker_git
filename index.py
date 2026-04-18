15c94699f6ea38220a6a46b577cc90c02f570ab3
Anusha <anusha4615@gmail.com>
2026-04-18 16:21:10 +0530

    secomd commit

--- index.py
+++ index.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello learning devops is easy but mastering need practice"
+    return "Hello learning devops is easy but mastering need practice and patience"
 if __name__ == "__main__":
     app.run(host = "0.0.0.0" ,port= 5000)
