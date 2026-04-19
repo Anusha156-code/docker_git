a8acb35558c1bc9a21eef63b226353aa6bc87349
Anusha <anusha4615@gmail.com>
2026-04-19 21:14:01 +0530

    checking after ngrok addition and updating webhook and understanding

--- index.py
+++ index.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello learning devops is easy but mastering need practice and patience and yes u will be on perfect path and ready..."
+    return "Hello learning devops is easy but mastering need practice and patience and yes u will be on perfect path and ready......next is mastering"
 if __name__ == "__main__":
     app.run(host = "0.0.0.0" ,port= 5000)
